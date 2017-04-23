c8860cf63104c826ef0a61c6c9a2ba54afb2e58a
sjmckinney <stevejmckinney@gmail.com>
2017-04-23 15:27:35 +0100

    Changed DriverTest to use beforeClass, Test & afterClass methods
    Added baseUrl property to POM & removed as field from class
    Changed fields and methods to static as required

--- src/test/java/com/_34protons/tests/DriverTest.java
+++ src/test/java/com/_34protons/tests/DriverTest.java
@@ -1,48 +1,62 @@
 package com._34protons.tests;
 
 import com._34protons.config.DriverType;
-import junit.framework.TestCase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com._34protons.config.DriverType.CHROME;
 import static com._34protons.config.DriverType.valueOf;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Simple test of driver creation code
  */
-public class DriverTest extends TestCase {
 
-    private DriverType defaultDriverType = CHROME;
+public class DriverTest {
+
+    private static DriverType defaultDriverType = CHROME;
     private final static Logger logger = LogManager.getLogger(DriverTest.class);
-    private WebDriver webDriver = null;
-    private String browser = null;
-    private String expectedPageTitle = "Google";
-    private String url = "https://www.google.com";
-    private String pageTitle = null;
+    private static WebDriver webDriver = null;
+    private String expectedPageTitle = "Adventures in automation | Thoughts about automated testing";
 
-    public void testChromeDriver()
-    {
+    @BeforeClass
+    public static void beforeClass() {
+
         DriverType driverType = getDefaultDriverType();
         DesiredCapabilities desiredCapabilities = driverType.getDesiredCapabilities();
         webDriver = driverType.getwebDriverObject(desiredCapabilities);
+        webDriver.get(System.getProperty("baseUrl", "http://www.34protons.co.uk"));
 
-        webDriver.get(url);
-        pageTitle = webDriver.getTitle();
+    }
+
+    @Test
+    public void testChromeDriver()
+    {
+        String pageTitle = webDriver.getTitle();
         logger.info("Browser opened url ".concat(webDriver.getCurrentUrl()));
         logger.info("Title of page is ".concat(pageTitle));
         assertEquals("Title of page is not as asserted", expectedPageTitle, pageTitle);
-        webDriver.quit();
         logger.info("Tests in DriverTest class passed");
     }
 
-    private DriverType getDefaultDriverType() {
+    @AfterClass
+    public static void AfterClass() {
+
+        webDriver.quit();
+
+    }
+
+    private static DriverType getDefaultDriverType() {
+
         DriverType driverType = defaultDriverType;
 
         try{
-            browser = System.getProperty("browser").toUpperCase();
+            String browser = System.getProperty("browser").toUpperCase();
             driverType = valueOf(browser);
         } catch (IllegalArgumentException ignored) {
             logger.error("Unknown driver specified. Reverting to default: ".concat(defaultDriverType.toString()));
